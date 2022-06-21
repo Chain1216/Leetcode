0871f61d030f73e7b94b1aaa37ef8f8f9924120c
Beaver_World <95999849+Chain1216@users.noreply.github.com>
2022-06-21 18:03:34 +0800

    Add files via upload

---
+++ For_fun.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class For_fun {
+	
+	public static void main(String args[]) {
+		int x = Integer.parseInt("-5");
+		System.out.println(x);
+		
+		
+		System.out.println(10/3);
+		
+		
+
+}
+}
